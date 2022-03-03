b078c6a861e1374d6f7e6098cef52c001d3caddf
Mikael Winter <54064888+michalwinter@users.noreply.github.com>
2022-03-03 10:43:30 +0100

    Configured manifest with icons

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  pwa: {
+    name: 'SSPŠaG Suplování',
+    themeColor: '#00497B',
+    msTileColor: '#00497B',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: "black-translucent",
+    icons: [
+      {
+        "src": "public/img/icons/manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "public/img/icons/manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "maskable"
+      },
+      {
+        "src": "public/img/icons/manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "public/img/icons/manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "maskable"
+      }
+    ]
+  }
+}
